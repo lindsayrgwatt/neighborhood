b34cce90b554e59fea703bce15f091707446e4f0
Lindsay Watt <lindsayrgwatt@gmail.com>
2013-05-01 10:21:09 -0700

    Updated fabfile to include Github push

--- neighborhood/fabfile.py
+++ neighborhood/fabfile.py
@@ -2,5 +2,5 @@ from fabric.api import local
 
 def prepare_deployment():
     local('python manage.py test hoods --settings=neighborhood.settings.local')
-    #local('git add -A && git commit')
-    #local('git push origin master')
+    local('git add -A && git commit')
+    local('git push origin master')
