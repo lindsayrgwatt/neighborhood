7c3307e0d1d3f58d5017f09d307ce70ead56afda
Lindsay Watt <lindsayrgwatt@gmail.com>
2013-05-01 10:27:59 -0700

    Updated documentation

--- neighborhood/fabfile.py
+++ neighborhood/fabfile.py
@@ -4,3 +4,4 @@ def prepare_deployment():
     local('python manage.py test hoods --settings=neighborhood.settings.local')
     local('git add -A && git commit')
     local('git push origin master')
+    
